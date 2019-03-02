4d83e161d628ca8e277e718b0f5e56f0493a85d0
kowaraj <kowaraj4stuff@gmail.com>
2019-03-02 13:08:11 +0100

    Run 2 nets and a router in between

--- centos_all/base_it/run_network.sh
+++ centos_all/base_it/run_network.sh
@@ -1,4 +1,13 @@
 #!/bin/bash 
+
+# host01 in net01
 docker run -it --name=kj_host01 --network=kj_net01 kowaraj/test:kj_base_it bash
+
+# host02 in net02
 docker run -it --name=kj_host02 --network=kj_net02 kowaraj/test:kj_base_it bash
+
+# router in between, connected to both net01 and net02
 docker run -it --name=kj_host03 --network=kj_net01 kowaraj/test:kj_base_it bash
+docker network connect kj_net02 kj_host03
+
+
